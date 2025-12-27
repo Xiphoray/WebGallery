b69d67eb3377e884e605fade946fb7f97352b609
Xiphoray <351014842@qq.com>
2025-12-27 09:55:18 +0800

    Add VitePWA plugin to Vite configuration

--- album-frontend/vite.config.js
+++ album-frontend/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+	VitePWA({
+      	registerType: 'autoUpdate',
+      	manifest: require('./public/manifest.json'),
+     	 workbox: {
+       	 // 可根据需要自定义缓存策略
+     	 }
+   	 })],
   base: './', // 或者 base: '' 确保所有资源使用相对路径
   server: {
     host: '0.0.0.0',
